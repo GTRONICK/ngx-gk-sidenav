627afd22c099ac3d80802b4b36845d1f0d279630
GTRONICK <gtronick@gmail.com>
2022-09-12 08:52:11 -0500

    Change selector name

--- projects/ngx-gk-sidenav/src/lib/ngx-gk-sidenav.component.ts
+++ projects/ngx-gk-sidenav/src/lib/ngx-gk-sidenav.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'lib-ngx-gk-sidenav',
+  selector: 'ngx-gk-sidenav',
   template: `
     <p>
       ngx-gk-sidenav works!
